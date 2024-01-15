3a9ca181b8cd6416cb7a25cdc041cd8bf7d4f81d
Md. Abdul Kader <abdulemail789@gmail.com>
2024-01-15 21:51:14 +0600

    created component, pages, styles, context folders and Home, Navbar component

---
+++ src/main.jsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import ErrorPage from "./error-page";
+import Main from "./pages/Main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/home",
+        element: <Home />
+      },
+
+
+    ]
+  }
+
+
+]);
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <AuthProvider>
+//       <RouterProvider router={router} />
+//     </AuthProvider>
+//   </React.StrictMode>
+// );
